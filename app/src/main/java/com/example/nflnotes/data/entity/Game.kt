645ff20cc6a35baad6ec774e313095e4bbdb1ed8
ponomarevss <s-siberia@yandex.ru>
2020-10-25 09:52:44 +0500

    MainActivity.kt - proof of concept

--- app/src/main/java/com/example/nflnotes/data/entity/Game.kt
+++ app/src/main/java/com/example/nflnotes/data/entity/Game.kt
@@ -4,5 +4,6 @@ data class Game(
     val hostName: String,
     val hostScore: String,
     val guestName: String,
-    val guestScore: String
+    val guestScore: String,
+    var isWatched: Boolean = false
 )
