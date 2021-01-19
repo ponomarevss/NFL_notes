c70c90355c5b9f0ed9cfc58fbfb50fe823c4d253
ponomarevss <s-siberia@yandex.ru>
2021-01-19 16:26:48 +0500

    getToken - private проверить работоспособность кода

--- app/src/main/java/com/example/nflnotes/mvp/model/repo/RetrofitDataRepo.kt
+++ app/src/main/java/com/example/nflnotes/mvp/model/repo/RetrofitDataRepo.kt
@@ -6,11 +6,12 @@ import com.example.nflnotes.mvp.model.entity.games.GamesQuery
 import com.example.nflnotes.mvp.model.entity.Token
 import com.example.nflnotes.mvp.model.entity.games.TeamsQuery
 import com.example.nflnotes.mvp.model.entity.response.GamesResponse
+import com.example.nflnotes.mvp.model.entity.response.TeamsResponse
 import com.google.gson.Gson
 import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.schedulers.Schedulers
 
-class RetrofitDataRepo(val api: IDataSource) : IDataRepo {
+class RetrofitDataRepo(private val api: IDataSource) : IDataRepo {
 
     val gson = Gson()
 
@@ -20,16 +21,46 @@ class RetrofitDataRepo(val api: IDataSource) : IDataRepo {
         AuthData.clientSecret
     ).subscribeOn(Schedulers.io())
 
-    override fun getGames(query: GamesQuery) = api.gamesResponse(
-        "Bearer ${getToken().blockingGet().accessToken}",
-        gson.toJson(query)
-        ).subscribeOn(Schedulers.io())
+    override fun getGames(query: GamesQuery): Single<GamesResponse> {
+        lateinit var gamesResponse: Single<GamesResponse>
+        getToken().subscribe({
+            gamesResponse = api.gamesResponse(
+                "Bearer ${it.accessToken}",
+                gson.toJson(query)
+            )
+        }, {
+            println("Can't get the accessToken: ${it.message}")
+        })
+        return gamesResponse
+    }
 
-    override fun getTeams(query: TeamsQuery) = api.teamsResponse(
-        "Bearer ${getToken().blockingGet().accessToken}",
-        gson.toJson(query)
-    ).subscribeOn(Schedulers.io())
+    override fun getTeams(query: TeamsQuery): Single<TeamsResponse> {
+        lateinit var teamsResponse: Single<TeamsResponse>
+        getToken().subscribe({
+            teamsResponse = api.teamsResponse(
+                "Bearer ${it.accessToken}",
+                gson.toJson(query)
+            )
+        }, {
+            println("Can't get the accessToken: ${it.message}")
+        })
+        return teamsResponse
+    }
 
+// Второй вариант
+//
+//    override fun getGames(query: GamesQuery) = api.gamesResponse(
+//        "Bearer ${getToken().blockingGet().accessToken}",
+//        gson.toJson(query)
+//        ).subscribeOn(Schedulers.io())
+//
+//    override fun getTeams(query: TeamsQuery) = api.teamsResponse(
+//        "Bearer ${getToken().blockingGet().accessToken}",
+//        gson.toJson(query)
+//    ).subscribeOn(Schedulers.io())
+
+// Первый вариант
+//
 //    override fun getGames(token: Token, query: GamesQuery) = api.gamesResponse(
 //        "Bearer ${token.accessToken}",
 //        gson.toJson(query)
@@ -39,5 +70,5 @@ class RetrofitDataRepo(val api: IDataSource) : IDataRepo {
 //        "Bearer ${token.accessToken}",
 //        gson.toJson(query)
 //    ).subscribeOn(Schedulers.io())
-//
+
 }
--- app/src/main/java/com/example/nflnotes/mvp/presenter/GamesPresenter.kt
+++ app/src/main/java/com/example/nflnotes/mvp/presenter/GamesPresenter.kt
@@ -1,8 +1,10 @@
 package com.example.nflnotes.mvp.presenter
 
 import com.example.nflnotes.mvp.model.entity.Game
-import com.example.nflnotes.mvp.model.entity.Token
+import com.example.nflnotes.mvp.model.entity.Team
 import com.example.nflnotes.mvp.model.entity.games.GamesQuery
+import com.example.nflnotes.mvp.model.entity.games.TeamsQuery
+import com.example.nflnotes.mvp.model.entity.query.Season
 import com.example.nflnotes.mvp.model.entity.query.Week
 import com.example.nflnotes.mvp.model.repo.IDataRepo
 import com.example.nflnotes.mvp.presenter.list.IGamesListPresenter
@@ -11,11 +13,12 @@ import com.example.nflnotes.mvp.view.list.IGameItemView
 import io.reactivex.rxjava3.core.Scheduler
 import moxy.MvpPresenter
 
-class GamesPresenter(val mainThreadScheduler: Scheduler, val repo: IDataRepo) :
+class GamesPresenter(private val mainThreadScheduler: Scheduler, private val repo: IDataRepo) :
     MvpPresenter<GamesView>() {
 
     class GamesListPresenter : IGamesListPresenter {
         val games = mutableListOf<Game>()
+        val teams = mutableListOf<Team>()
 
         override var itemClickListener: ((IGameItemView) -> Unit)? = null
 
@@ -23,29 +26,54 @@ class GamesPresenter(val mainThreadScheduler: Scheduler, val repo: IDataRepo) :
 
         override fun bindView(view: IGameItemView) {
             val game = games[view.pos]
-            view.setTeams(game.homeTeam?.id, game.visitorTeam?.id)
+            view.setTeams(
+                game.homeTeam?.id?.let { getTeamById(it)?.abbr },
+                game.visitorTeam?.id?.let { getTeamById(it)?.abbr }
+            )
             view.setScore(game.homeTeamScore?.pointsTotal, game.visitorTeamScore?.pointsTotal)
         }
+
+        private fun getTeamById(teamId : String) : Team? {
+            lateinit var team : Team
+            teams?.forEach {
+                if (it.id == teamId)
+                    team = it
+            }
+            return team
+        }
+
     }
 
-    val gamesListPresenter = GamesListPresenter()
+    private val gamesListPresenter = GamesListPresenter()
 
-    fun loadGames(query: GamesQuery) {
+    private fun getGames(query: GamesQuery) {
         repo.getGames(query)
             .observeOn(mainThreadScheduler)
             .subscribe({ gamesResponse ->
                 gamesListPresenter.games.clear()
                 gamesResponse.games?.let { it -> gamesListPresenter.games.addAll(it) }
             }, {
-                println("Can't get that GAMES: ${it.message}")
+                println("Can't get the gamesResponse: ${it.message}")
             })
         viewState.updateList()
+    }
+
+    private fun getTeams(query: TeamsQuery) {
+        repo.getTeams(query)
+            .observeOn(mainThreadScheduler)
+            .subscribe({teamsResponse ->
+                gamesListPresenter.teams.clear()
+                teamsResponse.teams?.let { gamesListPresenter.teams.addAll(it) }
+            }, {
+                println("Can't get teamsResponse: ${it.message}")
+            })
     }
 
     override fun onFirstViewAttach() {
         super.onFirstViewAttach()
         viewState.init()
-        loadGames(GamesQuery(Week(2018, "REG", 6)))
+        getGames(GamesQuery(Week(2018, "REG", 6)))
+        getTeams(TeamsQuery(Season(2018)))
         //todo: вводить данные из viewState
 
         gamesListPresenter.itemClickListener = { itemView ->
